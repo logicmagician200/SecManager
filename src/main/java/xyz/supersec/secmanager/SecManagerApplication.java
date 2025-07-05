fee9a0e6379034f39cfadfde08f2ae38722dfdcf
logicmagician <logicmagician@outlook.com>
2025-07-06 06:38:38 +0800

    初始化

---
+++ src/main/java/xyz/supersec/secmanager/SecManagerApplication.java
@@ -0,0 +1,16 @@
+package xyz.supersec.secmanager;
+
+import org.objectweb.asm.ClassWriter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecManagerApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecManagerApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/xyz/supersec/secmanager/SecManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.supersec.secmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
